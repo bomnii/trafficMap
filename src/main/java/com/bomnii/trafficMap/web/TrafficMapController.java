fded988b41e476042fcd4c2f18aaebc051fa108f
bomnii <m8nstruck0621@gmail.com>
2022-06-06 22:42:56 +0900

    first commit.

---
+++ src/main/java/com/bomnii/trafficMap/web/TrafficMapController.java
@@ -0,0 +1,15 @@
+package com.bomnii.trafficMap.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TrafficMapController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World";
+	}
+}
